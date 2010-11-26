1a2502c661c9779571d841104fe498adabac98b2
Don Zaragoza <donzaragoza1975@yahoo.com>
2010-11-26 13:31:43 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cherryamor::Application.config.secret_token = 'c988f2038e874324deb7b2e7712fe17378c49b881c8403fc1c764b0e6174e31aac5da5fd673f7e6df42db8683a5178ccf3ec9c4364f5699734061ffeb71f77d5'
